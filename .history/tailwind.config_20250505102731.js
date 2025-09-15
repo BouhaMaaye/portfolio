3348fafe2f1c6ad9ae2bb20dff4bb0535f5bfa2b
fernandineho <fernandineho@gmail.com>
2025-09-15 11:53:31 +0200

    Ajout de nouvelles fonctionnalités

--- .history/tailwind.config_20250505102731.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Remplacez 'purge' par 'content'
-    theme: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/forms')],
-  };
